6424344519fdd411e7a5a10f991523d296715db4
ShanilkaDharmapriya <161996037+ShanilkaDharmapriya@users.noreply.github.com>
2025-01-02 18:59:15 +0530

    back-end finish

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,8 +1,8 @@
 import express from 'express';
 
 const articleinfo = [
-    {articleName:'learn-react',upvotes:0},
-    {articleName:'Mongoth',upvotes:0}
+    {articleName:'learn-react',upvotes:0,comments:[]},
+    {articleName:'Mongoth',upvotes:0,comments:[]}
 ]
 
 const app = express();
@@ -12,7 +12,21 @@ app.use(express.json());
 app.post('/api/articles/:name/upvotes',(req,res)=>{
     const article=articleinfo.find(a=> a.articleName === req.params.name);
     article.upvotes += 1;
-    res.send({upvotes: article.upvotes})
+    res.json(article)
+})
+
+app.post('/api/articles/:name/comments',(req,res)=>{
+    const {name} = req.params;
+    const {postedby,text}= req.body;
+
+    const article=articleinfo.find(a=>a.articleName===name);
+
+    article.comments.push({
+        postedby,
+        text
+    })
+    res.json(article);
+
 })
 
 app.listen(8000 , function(){
